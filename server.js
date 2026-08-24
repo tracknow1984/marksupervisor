559743659cc87801f2c527de5b871ac1f8b0a9b8
tracknow1984 <mark@tracknow.com.au>
2026-08-24 22:33:07 +1000

    Point compatibility launcher to Supervisor365 v5

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 // Compatibility entry point for hosting services configured to run `node server.js`.
-// The current Supervisor365 application lives in app-v4.js.
-require('./app-v4.js');
+// The current Supervisor365 application lives in app-v5.js.
+require('./app-v5.js');
