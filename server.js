01cc092d002834695933a645969a9bd10ec84eb7
tracknow1984 <mark@tracknow.com.au>
2026-08-25 21:55:37 +1000

    Enable smart grouped Supervisor365 navigation

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 // Stable hosting entry point for Supervisor365.
+// Install the smart grouped navigation before the app mounts its routes.
+require('./src/smart-nav-patch');
 // All pages are now mounted from isolated route modules under src/routes.
 require('./app-master.js');
